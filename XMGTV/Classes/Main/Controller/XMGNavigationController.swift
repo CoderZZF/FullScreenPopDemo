3ec92700ee27a50bf65a9d9d7a1a4c65e93ae727
zhangzhifu <595373131@qq.com>
2017-02-24 00:27:41 +0800

    全局popgon

--- XMGTV/Classes/Main/Controller/XMGNavigationController.swift
+++ XMGTV/Classes/Main/Controller/XMGNavigationController.swift
@@ -13,24 +13,33 @@ class XMGNavigationController: UINavigationController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // 1.遍历所有中的属性, 找到需要的属性
-        /*
-        var count : UInt32 = 0
-        let ivars = class_copyIvarList(UIGestureRecognizer.self, &count)!
-        for i in 0..<count {
-            let ivar = ivars[Int(i)]
-            let name = ivar_getName(ivar)!
-            let nameStr = String(cString: name)
-            print(nameStr)
-        }
-         */
+        // 1. 校验手势是否有值
+        // target/action
+        guard let interactivePop = interactivePopGestureRecognizer else { return }
         
-        guard let targets = interactivePopGestureRecognizer?.value(forKey: "_targets") as? [NSObject] else { return }
-        let targetObjc = targets[0]
-        let target = targetObjc.value(forKey: "target")
+        
+//        // 2. 通过运行时机制找到想要的属性
+//        var count : UInt32 = 0
+//        let ivars = class_copyIvarList(UIGestureRecognizer.self, &count)!
+//        for i in 0..<count {
+//            let ivar = ivars[Int(i)]
+//            let nameP = ivar_getName(ivar)
+//            let name = String(cString: nameP!)
+//            print(name)
+//        }
+        
+        guard let values = interactivePop.value(forKeyPath: "_targets") as? [NSObject] else { return }
+        
+        guard let objc = values.first else { return }
+        
+//         3. 取出action/target
+//        print(objc)
+        let target = objc.value(forKeyPath: "target")
         let action = Selector(("handleNavigationTransition:"))
+//        let action = objc.value(forKeyPath: "action") as? Selector
         
-        // 创建拖拽手势, 并且添加到导航控制器的View中
+        
+//        // 2. 创建自己的手势
         let panGes = UIPanGestureRecognizer(target: target, action: action)
         view.addGestureRecognizer(panGes)
     }
